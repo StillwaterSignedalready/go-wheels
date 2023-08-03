a11075b7ac517e70812dfc7e2de49e5515129098
canglang <doublecross@yeah.net>
2023-08-03 11:30:10 +0800

    yes complete

--- yes/yes.go
+++ yes/yes.go
@@ -2,11 +2,24 @@ package yes
 
 import (
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func Chatter() {
+	sigChan := make(chan os.Signal, 1)
+	stopTiming := time.Now().Add(time.Second * 3)
 
-	for {
+	signal.Notify(sigChan, syscall.SIGPIPE, syscall.SIGINT, os.Interrupt)
+	go func() {
+		<-sigChan
+		os.Exit(0) // no print
+	}()
+
+	for time.Now().Before(stopTiming) { // while()
 		os.Stdout.Write([]byte("y\n"))
+		// os.Stdout.Sync() // flush buffer content to terminal
 	}
+
 }
